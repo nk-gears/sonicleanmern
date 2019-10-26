23ab3ce0653305996c69fc6ece9e6c9964f7a204
wonderdev21 <wonder.dev21@gmail.com>
2019-10-26 11:55:14 +0900

    fix server.js

--- server.js
+++ server.js
@@ -32,6 +32,12 @@ app.use('/public', cors(publicOptions))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // DB Config
 const db = process.env.MONGODB_URI || require("./config/keys").mongoURI;
 
