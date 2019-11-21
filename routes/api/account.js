aab3fa4bb888d785ef7c99f3d860ac1b462d13cb
wonderdev21 <wonder.dev21@gmail.com>
2019-11-21 10:48:03 +0900

    updated account issues

--- routes/api/account.js
+++ routes/api/account.js
@@ -29,12 +29,12 @@ router.get(
 
       data.mainstore = {};
       data.mainstore.name = user.stores[0].name;
-      (data.mainstore.phoneNumber = user.stores[0].phoneNumber),
-        (data.mainstore.address1 = user.stores[0].address1),
-        (data.mainstore.address2 = user.stores[0].address2),
-        (data.mainstore.city = user.stores[0].city),
-        (data.mainstore.us_state = user.stores[0].us_state),
-        (data.mainstore.zipcode = user.stores[0].zipcode);
+      data.mainstore.phoneNumber = user.stores[0].phoneNumber;
+      data.mainstore.address1 = user.stores[0].address1;
+      data.mainstore.address2 = user.stores[0].address2;
+      data.mainstore.city = user.stores[0].city;
+      data.mainstore.us_state = user.stores[0].us_state;
+      data.mainstore.zipcode = user.stores[0].zipcode;
       res.json(data);
     });
   }
@@ -53,28 +53,43 @@ router.put(
     }
 
     User.findById(req.params.id).then(user => {
-      (user.firstName = req.body.firstName),
-        (user.lastName = req.body.lastName),
-        (user.email = req.body.email),
-        (user.phoneNumber = req.body.phoneNumber),
-        (user.workPhoneNumber = req.body.workPhoneNumber),
-        (user.extension = req.body.extension),
-        user
-          .save()
-          .then(user => {
-            let data = {};
-            data._id = user._id;
-            data.firstName = user.firstName;
-            data.lastName = user.lastName;
-            data.email = user.email;
-            data.phoneNumber = user.phoneNumber;
-            data.workPhone = user.workPhone;
-            data.extension = user.extension;
-            (data.workPhoneNumber = user.workPhoneNumber),
-              (data.userPhoto = user.userPhoto);
-            res.json(data);
-          })
-          .catch(err => console.log(err));
+      user.firstName = req.body.firstName;
+      user.lastName = req.body.lastName;
+      user.email = req.body.email;
+      user.phoneNumber = req.body.phoneNumber;
+      user.workPhoneNumber = req.body.workPhoneNumber;
+      user.extension = req.body.extension;
+      user
+        .save()
+        .then(user => {
+          let data = {};
+          data._id = user._id;
+          data.firstName = user.firstName;
+          data.lastName = user.lastName;
+          data.email = user.email;
+          data.phoneNumber = user.phoneNumber;
+          data.workPhone = user.workPhone;
+          data.extension = user.extension;
+          data.workPhoneNumber = user.workPhoneNumber;
+          data.userPhoto = user.userPhoto;
+          data.roles = user.roles;
+          data.storesCount = user.stores.length;
+          data.mohawkBrand = user.mohawkBrand;
+          data.companyName = user.companyName;
+          data.mohawkAccount = user.mohawkAccount;
+          data.created = user.created;
+
+          data.mainstore = {};
+          data.mainstore.name = user.stores[0].name;
+          data.mainstore.phoneNumber = user.stores[0].phoneNumber;
+          data.mainstore.address1 = user.stores[0].address1;
+          data.mainstore.address2 = user.stores[0].address2;
+          data.mainstore.city = user.stores[0].city;
+          data.mainstore.us_state = user.stores[0].us_state;
+          data.mainstore.zipcode = user.stores[0].zipcode;
+          res.json(data);
+        })
+        .catch(err => console.log(err));
     });
   }
 );
